2abb5b0e188a06b8d7e75f3cd5975f8e5a9d8578
MrAbderrahmane <mr.abderrahmane@gmail.com>
2023-02-25 13:11:02 +0000

    adding constants

---
+++ js/constants.js
@@ -0,0 +1,47 @@
+const CONSTANTS = {
+  WIDTH: 400, 
+  HEIGHT: 400,
+  ROWS: 4, 
+  COLS: 4,
+  PADDING: 50,
+  SQUARE_SIZE: 50,
+  RADIUS: 10,
+  VALIDMOVERADIUS: 4, 
+  ITER: 2,
+
+  FIRSTPLAYER: 1,
+  FIRSTPLAYERKING: 1.1,
+  SECONDPLAYER: -1,
+  SECONDPLAYERKING: -1.1,
+  FIRSTPLAYERCOLOR: 'rgb(255, 0, 0)',
+  SECONDPLAYERCOLOR: 'rgb(255, 255, 255)',
+  HIGHLIGHTCOLOR: 'rgba(128, 128, 128, .5)',
+  VALIDMOVECOLOR: 'green',
+
+  COLORS: {
+    [this.FIRSTPLAYER]:this.FIRSTPLAYERCOLOR,
+    [this.SECONDPLAYER]:this.SECONDPLAYERCOLOR
+  },
+
+  ALLNEIBOURS: [
+    {row:-1,col:0}, // 0 up
+    {row:-1,col:1}, // 1 up right
+    {row:0,col:1},  // 2 right
+    {row:1,col:1},  // 3 down right
+    {row:1,col:0},  // 4 down
+    {row:1,col:-1}, // 5 down left
+    {row:0,col:-1}, // 6 left
+    {row:-1,col:-1},// 7 up left
+  ],
+
+
+  PIECECOAFFICIENT: 1,
+  KINGBONUSCOAFFICIENT: 10,
+
+  AITIMELIMIT: 60 * 1000,
+}
+CONSTANTS.CELLWIDTH = CONSTANTS.SQUARE_SIZE * 2;
+CONSTANTS.COLORS = {
+  [CONSTANTS.FIRSTPLAYER]:CONSTANTS.FIRSTPLAYERCOLOR,
+  [CONSTANTS.SECONDPLAYER]:CONSTANTS.SECONDPLAYERCOLOR
+};
